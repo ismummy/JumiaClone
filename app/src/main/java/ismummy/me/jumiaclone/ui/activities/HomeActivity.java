4a7371698a163de11e6120705e43a578da22e745
ismummy <irolalekan@gmail.com>
2018-04-10 23:47:21 +0100

    Jumia Clone app designed for michosyst as a second phase to her interview. The project started today 10th of april 2018, after ma dope exam yesterday :)

---
+++ app/src/main/java/ismummy/me/jumiaclone/ui/activities/HomeActivity.java
@@ -0,0 +1,16 @@
+package ismummy.me.jumiaclone.ui.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import ismummy.me.jumiaclone.R;
+import ismummy.me.jumiaclone.ui.base.BaseActivity;
+
+public class HomeActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
